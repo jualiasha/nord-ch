8e75ad2a2f447e4656d3cdfb72fa3d48d2b2e45b
jualiasha <jualiasha@gmail.com>
2024-01-27 02:10:54 +0200

    vitests installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import rawPlugin from 'vite-raw-plugin';
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { defineConfig } from "vitest/config";
+import rawPlugin from "vite-raw-plugin";
 
-export default {
-    plugins: [
-        rawPlugin({fileRegex: /\.md$/}) // Ability to read markdown files.
-    ]
-}
+const config = defineConfig({
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      name: "chrome",
+    },
+    coverage: {
+      provider: "istanbul",
+      include: ["src/**/*.ts", "src/**/*.js"],
+    },
+  },
+  plugins: [
+    nodePolyfills(), // Storybook/vitest workaround
+    rawPlugin({ fileRegex: /\.md$/ }), // Ability to read markdown files.
+  ],
+});
+
+export default config;
